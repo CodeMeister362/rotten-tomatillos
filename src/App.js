56c287e86512db2a42fcb221ae929c4575240c95
Em Lindvall <elindvall22@gmail.com>
2023-05-17 19:56:55 -0500

    refac: Refactor implementation code and router imports to sync routes with browser back button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css'
 
 // component imports 
 import React from 'react';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Header from './Components/Header'
 import Spinner from './Components/Spinner'
 import PosterGrid from './Components/PosterGrid'
@@ -14,36 +14,40 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      movies: null,
-      singleMovie: null
+      movies: [],
+      singleMovie: []
     }
   }
 
 // lifecycle methods 
   componentDidMount = () => {
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
-        .then((response) => response.json())
-        .then((data) => this.setState({movies: data}))
-        .catch((err)  => this.setState({error: err.message})
-        )
-      }
+      .then((response) => response.json())
+      .then((data) => this.setState({movies: data}))
+      .catch((err)  => this.setState({error: err.message})
+      )
+    }
 
 // functions
   getMovie = (id) => {
-    console.log(id)
     const movie = this.state.movies.movies.filter(movie => {
       return movie.id === id
     })
     this.setState({ singleMovie: movie })
   }
 
+  setSelectedView = ()  =>  {
+    this.setState({ singleMovie: [] })
+  }
+
   backButton = () =>  {
-    this.setState({ singleMovie: null })
+    this.setState({ singleMovie: [] })
   }
 
 // component render
   render() {
-    if(this.state.movies === null) {
+    console.log(this.state);
+    if(this.state.movies.length === 0) {
       return(
           <main className="App">
             <Route 
@@ -57,12 +61,12 @@ class App extends React.Component {
             }/>
           </main>
       )
-    }
-    if(this.state.singleMovie === null) {
-      return (
+    } else {
+      return(
         <main className="App">
-          <Route 
-              path="/"
+          <Switch>
+            <Route 
+              exact path="/"
               render = {() => (
                 <div>  
                   <Header />
@@ -71,27 +75,22 @@ class App extends React.Component {
                     getMovie={this.getMovie}
                   />
                 </div>
-              )
-            }/>
-        </main>
-    );
-    } else {
-      return (
-        <main className="App">
-          <Route 
-            path="/:id"
-            render = {({ match })  =>  {
-              return(
-                <div>
-                <Header />
-                <SelectedMovie 
-                movie={this.state.singleMovie}
-                backButton={this.backButton} /> 
-              </div>
-              )
-              }}
-          />  
-        </main>
+              )}/>
+            <Route 
+              path="/:id"
+              render = {({ match })  =>  {
+                return(
+                  <div>
+                  <Header />
+                  <SelectedMovie 
+                  key={match.params.id}
+                  movie={this.state.singleMovie}
+                  backButton={this.backButton} 
+                  />
+                </div>
+                )}}/>  
+            </Switch>
+          </main>
       )
     }
   }
--- src/Components/SelectedMovie.js
+++ src/Components/SelectedMovie.js
@@ -6,47 +6,27 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 // component 
-const SelectedMovie = ({movie, backButton, URL}) => {
+const SelectedMovie = ({movie, backButton}) => {
   let loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  URL = (`${movie[0].id}`);
-  console.log(URL);
 
 	return(
     <div className="selected-movie">
-      {/* <Route
-        exact path="/:URL"
-        render = {()  =>  ( */}
-          <div className="selected-movie">
-            <img className="background" src={movie[0].backdrop_path}/>
-            <div className="gradient"/>
-            <div className="details-container">
-              <img className="poster" src={movie[0].poster_path}/>
-              <div className="details">
-                <h1 className="title">{movie[0].title}</h1>
-                <p className="release-date">{movie[0].release_date.slice(0,4)}</p>
-                <p className="average-rating">{movie[0].average_rating.toFixed(0)} ⭐️</p>
-                <p className="description">{loremIpsum}</p>
-              </div>
-            </div>
-            <Link to={"/"}>
-              <button className="back-button" onClick={backButton}>back</button>
-            </Link>
+      <div className="selected-movie">
+        <img className="background" src={movie[0].backdrop_path}/>
+        <div className="gradient"/>
+        <div className="details-container">
+          <img className="poster" src={movie[0].poster_path}/>
+          <div className="details">
+            <h1 className="title">{movie[0].title}</h1>
+            <p className="release-date">{movie[0].release_date.slice(0,4)}</p>
+            <p className="average-rating">{movie[0].average_rating.toFixed(0)} ⭐️</p>
+            <p className="description">{loremIpsum}</p>
           </div>
-        {/* )}
-        /> */}
-    {/* <img className="background" src={movie[0].backdrop_path}/>
-      <div className="gradient"/>
-      <div className="details-container">
-        <img className="poster" src={movie[0].poster_path}/>
-        <div className="details">
-          <h1 className="title">{movie[0].title}</h1>
-          <p className="release-date">{movie[0].release_date.slice(0,4)}</p>
-          <p className="average-rating">{movie[0].average_rating.toFixed(0)} ⭐️</p>
-        <p className="description">{loremIpsum}</p>
         </div>
+        <Link to={"/"}>
+          <button className="back-button" onClick={backButton}>back</button>
+        </Link>
       </div>
-      <button className="back-button" onClick={backButton}>back</button>
-		</div> */}
     </div>
 	)
 }
