6b5ab2304d60b5cb745f79ac185fcb8f5cb07278
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-05-19 14:00:22 -0700

    Adds a search bar in the header. work in progress

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,14 +7,17 @@ import { Link } from 'react-router-dom'
 import React from 'react'
 
 // component
-const Header = ()  =>  {
+const Header = () => {
+
   return(
     <Link to={'/'}>
-    <div className="header-container">
+    <div>
       <img className="logo" src={logo} alt="rotten-tomatillos-logo"/>
     </div>
     </Link>
   )
 }
+
+//header will have to be a class component bc the state will have to change in order to understand that something is in the input fields 
 
 export default Header
