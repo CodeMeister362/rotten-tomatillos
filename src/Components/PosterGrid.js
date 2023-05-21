fe62b193d2300a9aa97dc04210fde1151eed6384
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-05-21 13:42:53 -0700

    Adds proptype checking and code refactor

--- src/Components/PosterGrid.js
+++ src/Components/PosterGrid.js
@@ -9,28 +9,28 @@ import SearchResult from './SearchResult'
 
 // functional component 
 const PosterGrid = ({ movies, getMovie, searchedMovie, searchInput}) =>  {  
-    const posters = (searchedMovie.length === 0 ? movies : searchedMovie).map(movie => {
-      return(
-        <Poster
-          key={movie.id}
-          id={movie.id}
-          title={movie.title}
-          posterPath={movie.poster_path}
-          getMovie={getMovie}
-        />
-      )
-    })
-    if (posters.length === 40 && searchInput === "yes") {
-      return(
-        <SearchResult />
-      )
-    } else {
-      return(
-        <div className ='poster-container'>
-          {posters}
-        </div>
-      )
-    }
+  const posters = (searchedMovie.length === 0 ? movies : searchedMovie).map(movie => {
+    return(
+      <Poster
+        key={movie.id}
+        id={movie.id}
+        title={movie.title}
+        posterPath={movie.poster_path}
+        getMovie={getMovie}
+      />
+    )
+  })
+  if (posters.length === 40 && searchInput === "yes") {
+    return(
+      <SearchResult />
+    )
+  } else {
+    return(
+      <div className ='poster-container'>
+        {posters}
+      </div>
+    )
+  }
 }
 
 export default PosterGrid
@@ -38,5 +38,7 @@ export default PosterGrid
 // prop types
 PosterGrid.propTypes = {
   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-  getMovie: PropTypes.func.isRequired
+  getMovie: PropTypes.func.isRequired,
+  searchedMovie: PropTypes.array.isRequired,
+  searchInput: PropTypes.string.isRequired
 }
